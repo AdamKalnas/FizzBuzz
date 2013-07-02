5b7cc56976f18c911863e8026130a76cadcf698e
Adam Kalnas <a.kalnas@modcloth.com>
2013-07-02 17:03:13 -0400

    seperating these tests out, and making it easier to setup new tests

--- fizz_buzz_specs.rb
+++ fizz_buzz_specs.rb
@@ -1,10 +1,23 @@
 require_relative 'fizz_buzz.rb'
 
 describe FizzBuzz, "Count" do
+  before(:each) do
+    @fizzbuzz = FizzBuzz.new
+  end
+
   it "Play FizzBuzz!" do
-    fizzbuzz = FizzBuzz.new
-    fizzbuzz.next.should == 1
-    fizzbuzz.next.should == 2
-    fizzbuzz.next.should == "fizz"
+    @fizzbuzz.next.should == 1
+    @fizzbuzz.next.should == 2
+  end
+
+  it 'should fizz on numbers that are evenly divided by 3' do
+    count(2)
+    @fizzbuzz.next.should == "fizz"
+  end
+
+  def count(i)
+    i.times do
+      @fizzbuzz.next
+    end
   end
 end
