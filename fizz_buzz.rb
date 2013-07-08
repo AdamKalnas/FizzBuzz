aa3f76c3106788b9094cabe5470392fa8c1409c9
Adam Kalnas <a.kalnas@modcloth.com>
2013-07-08 10:52:29 -0400

    We wont need the return statements if we can roll all the logic a bit cleaner

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -2,20 +2,17 @@ class FizzBuzz
   def initialize
     @count = 0
   end
-  
+
   def next
     @count += 1
-    
-    if @count % 3 == 0 && @count % 5 == 0
-      return 'fizzbuzz'
-    elsif @count % 3 == 0
-      return 'fizz'
-    elsif @count.to_s.include? '3'
-      return 'fizz'
+    if @count % 3 == 0 and @count % 5 == 0
+      'fizzbuzz'
+    elsif @count % 3 == 0 or @count.to_s.include? '3'
+      'fizz'
     elsif @count % 5 == 0 or @count.to_s.include? '5'
-      return 'buzz'
+      'buzz'
+    else
+      @count
     end
-
-    @count
   end
 end
